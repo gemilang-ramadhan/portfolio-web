59e893065acfb6f7b5cc3851c49dceed2b113629
Gemilang <gemilangramadhan000@gmail.com>
2024-10-02 22:07:30 +0700

    created web

---
+++ README.md
@@ -0,0 +1,3 @@
+# main-portfolio
+updated portfolio website 
+created using HTML, TailWind CSS, JS
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        primary: '#1DA1F2',
+        secondary: '#14171A',
+      },
+    }
+  },
+  plugins: [],
+
+}
+
